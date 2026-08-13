9ec7994508ad2052e0156b12989359073e7f17d1
Costing <enquiry@semcogroups.com>
2026-08-14 00:17:21 +0530

    fix: explicit CORS headers and progress email permissions for active users in index.js

--- index.js
+++ index.js
@@ -23,6 +23,15 @@ let dbSeeded = false;
 
 // Middleware
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(morgan('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
